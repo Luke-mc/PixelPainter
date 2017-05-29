019359e452ed82f0b301e78ca3eb6398454605de
Luke-mc <luke_mcglynn@yahoo.com>
2017-05-29 00:24:35 -1000

    cleaned up code

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,21 +1,23 @@
+// Naming PixelPainter Div
 var main = document.getElementById("pixelPainter");
-/* Creating PP-canvas*/
+//Creating PP-canvas
 var canvas = document.createElement("div");
 canvas.setAttribute("id", "pp-canvas");
-/* Creating Table */
+// Creating Table
 var table = document.createElement('table');
 table.setAttribute("classname", "tables");
-/* Appending */
-main.appendChild(canvas);
-canvas.appendChild(table);
+// Creating Controls
 var controls = document.createElement("div");
 controls.setAttribute("id", "controls");
-
+// Appending all
+main.appendChild(canvas);
+canvas.appendChild(table);
 document.body.appendChild(controls);
 
-
+// Loaded color
 var loadedColor = null;
 
+// Color Array
 var colorArray = [
 "#51574a",    "#447c69",   "#74c493",
 "#8e8c6d",    "#e4bf80",   "#e9d78e",
@@ -26,33 +28,29 @@ var colorArray = [
 "#7c9fb0",    "#5698c4",   "#9abf88"
 ];
 
-
-
-
-/*"Pixel Painter function */
+        // Pixel Painter function //
 function pixelPainter(width,height) {
 
-/* Generating Graph */
+        // Generating Graph and Swatch //
+
+//Creating Rows and Pixels
   for( var i = 0; i <= height; i++ ){
-/* Creating Row */
-   var tRow = document.createElement('tr');
-   table.appendChild(tRow);
+    var tRow = document.createElement('tr');
+    table.appendChild(tRow);
 
-
-   for( var j = 0; j <= width; j++ ){
-/* Creating Pixel */
-   var pixelTd =document.createElement('td');
-   //var pixel = document.createElement("div");
-   pixelTd.setAttribute("class", "pixelTd");
-   //pixel.setAttribute("class","pixel");
-   //pixel.addEventListener('mouseover', change);
-   pixelTd.addEventListener('mouseover', change);
-   tRow.appendChild(pixelTd);
-   //pixelTd.appendChild(pixel);
+    for( var j = 0; j <= width; j++ ){
+      var pixelTd =document.createElement('td');
+      //var pixel = document.createElement("div");
+      pixelTd.setAttribute("class", "pixelTd");
+      //pixel.setAttribute("class","pixel");
+      //pixel.addEventListener('mouseover', change);
+      pixelTd.addEventListener('mouseover', change);
+      tRow.appendChild(pixelTd);
+      //pixelTd.appendChild(pixel);
     }
-   }
+  }
 
-/* Color Swatch */
+// Creating Color Swatch
   for( var m = 0; m <= 5; m++ ){
     var colorDiv = document.createElement("div");
     var colTab = document.createElement('table');
@@ -60,64 +58,63 @@ function pixelPainter(width,height) {
     controls.appendChild(colorDiv);
     colorDiv.appendChild(colTab);
 
-
-      for( var l = 0; l < 21; l++ ){
-    var colRow = document.createElement("tr");
-    colRow.id = "swatch" + l ;
-    colRow.style.backgroundColor = colorArray[l];
-    colRow.addEventListener("click", choose);
-    colRow.innerHTML = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
-    colTab.appendChild(colRow);
-   }
-
+    for( var l = 0; l < 21; l++ ){
+      var colRow = document.createElement("tr");
+      colRow.id = "swatch" + l ;
+      colRow.style.backgroundColor = colorArray[l];
+      colRow.addEventListener("click", choose);
+      colRow.innerHTML = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+      colTab.appendChild(colRow);
+    }
   }
+        // Creating Buttons //
 
+// Button Div
   var btnDiv = document.createElement("div");
-    btnDiv.setAttribute("id", "btnDiv");
-    controls.appendChild(btnDiv);
+  btnDiv.setAttribute("id", "btnDiv");
+  controls.appendChild(btnDiv);
 
- //var penDiv = document.createElement("button//");
- //   penDiv.setAttribute("id", "penDiv");
- //   controls.appendChild(penDiv);
- //   penDiv.innerHtml = '<p>i am pen</p>';
+// Pen Size Button
+  //var penDiv = document.createElement("button//");
+  //penDiv.setAttribute("id", "penDiv");
+  //controls.appendChild(penDiv);
+  //penDiv.innerHtml = '<p>i am pen</p>';
 
-
-/* Clear Button */
+// Clear Button
   var clearBtn = document.createElement("button");
-   clearBtn.setAttribute("id", "clear");
-   clearBtn.addEventListener("mousedown", clear);
-   clearBtn.innerHTML = "Clear";
-   btnDiv.appendChild(clearBtn);
+  clearBtn.setAttribute("id", "clear");
+  clearBtn.addEventListener("mousedown", clear);
+  clearBtn.innerHTML = "Clear";
+  btnDiv.appendChild(clearBtn);
 
-/* Erase Button */
+// Erase Button
   var eraseBtn = document.createElement("button");
-    eraseBtn.setAttribute("id", "erase");
-    eraseBtn.addEventListener("click",erase);
-    eraseBtn.innerHTML = "Erase";
-    btnDiv.appendChild(eraseBtn);
+  eraseBtn.setAttribute("id", "erase");
+  eraseBtn.addEventListener("click",erase);
+  eraseBtn.innerHTML = "Erase";
+  btnDiv.appendChild(eraseBtn);
 
+        // Functions //
 
-function change(){
-  this.style.backgroundColor = loadedColor;
+// Change Pixels to LoadedColor
+  function change(){
+    this.style.backgroundColor = loadedColor;
+  }
+// Erase
+  function erase(){
+    loadedColor = "white";
+  }
+// Clear
+  function clear(){
+    document.getElementsByClassName("pixel").style.backgroundColor = "white";
+  }
+// Set Loaded Color
+  function choose(){
+    loadedColor = this.style.backgroundColor;
+  }
 }
 
-function erase(){
-  loadedColor = "white";
-}
-
-function clear(){
-  document.getElementsByClassName("pixel").style.backgroundColor = "white";
-}
-
-
-function choose(){
-  loadedColor = this.style.backgroundColor;
-}
-
-
-
-}
-
+// Invoke pixelPainter
 pixelPainter(70,120);
 
 
